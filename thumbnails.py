f216a1a3a0dcd8dc62ccdcd690ca29f483897d45
Lugburz <lugburz@operamail.com>
2019-10-28 02:05:32 +0100

    using thumbnails now, also two new comics

---
+++ thumbnails.py
@@ -0,0 +1,16 @@
+import glob, os, sys
+from PIL import Image
+
+size = 300, 300
+
+os.chdir("./img/comics")
+for file in glob.glob("*.jpg"):
+    outfile =  "./thumbnails/" + os.path.basename(file)
+    print(outfile)
+    if outfile != file:
+        try:
+            im = Image.open(file)
+            im.thumbnail(size, Image.ANTIALIAS)
+            im.save(outfile, "JPEG")
+        except IOError:
+            print("cannot create thumbnail for '%s'" % file)
